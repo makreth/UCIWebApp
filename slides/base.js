c8d0f6bf28accd7e715d9751c861db1c206cde8a
makreth <38960776+makreth@users.noreply.github.com>
2019-10-04 20:44:38 -0700

    Add files via upload

---
+++ slides/base.js
@@ -0,0 +1,54 @@
+const testing = [
+    {key: 0, content: getPage1()},
+    {key: 1, content: getPage2()},
+    {key: 2, content: getPage3()}
+]
+
+function applyBase(slides){
+    const base = `
+<div id="mainSlides" class="carousel slide" data-ride="carousel" data-interval="false">
+    <div class="carousel-inner">
+    ${slides.map(slide=>`
+        <div class="carousel-item">
+            <div class="container slide">
+                ${slide.content}
+            </div>
+        </div>
+    `
+    ).join('')}
+    </div>
+    <a class="carousel-control-prev" href="#mainSlides" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#mainSlides" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+    </a>
+</div>
+    `
+    return base;
+}
+console.log(applyBase(testing));
+document.getElementById("navbarJS").innerHTML = getNavBar();
+document.getElementById("content").innerHTML = applyBase(testing);
+document.getElementsByClassName("carousel-item")[0].classList.add("active");
+
+$('#mainSlides').on('slide.bs.carousel', function (e) {
+    const items = document.getElementsByClassName("nav-item");
+    items[e.from].classList.remove("active");
+    items[e.to].classList.add("active");
+});
+
+(function(){
+    const items = document.getElementsByClassName("nav-item");
+    for(let i = 0; i < items.length; i++){
+        items[i].addEventListener("click", function(e){
+            document.getElementsByClassName("nav-item active")[0].classList.remove('active');
+            $('.carousel').carousel(i);
+        });
+    }
+})();
+
+this.carouselNormalization();
+
---
+++ slides/page2.js
@@ -0,0 +1,20 @@
+function getPage2(){
+    const page2 = `
+    <h1>Why did I apply to this position?</h1>
+    <p>I'm applying to this position to get professional experience as a web developer. I have plenty of hobbyist experience with
+    programming in general, and I also taught HTML/CSS/JS professionally, but I never worked directly as a web developer.
+    I would love to take up the opportunity to design and build user interfaces that meet UCI's high standards, while taking steps towards
+    building a career that I can be proud of.</p>
+    <p>
+    Front-end development for me is a perfect mix between creativity and logic. Using the programming tools available to me,
+    I can utilize them to bolster my creative expression. One such example of this is using Scalable Vector Graphics (SVG) to
+    add an extra dimension of interactivity to any website, expressing strokes and shapes using code.
+    </p>
+    <p>Below is an SVG image that I designed for my portfolio in progress.</p>
+    <div class="svg-container">
+        <object class="slide-icon" type="image/svg+xml" data="logo.svg">
+        </object>
+    </div>
+    `
+    return page2;
+}
