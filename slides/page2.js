7cb5657194c166f3b39661cca367b5fabbe21a5a
Duncan Shepherd <38960776+makreth@users.noreply.github.com>
2020-01-29 11:52:34 -0800

    changed message

--- slides/page2.js
+++ slides/page2.js
@@ -1,12 +1,13 @@
 function getPage2(){
     const page2 = `
     <h1>Why did I apply to this position?</h1>
-    <p>I'm applying to this position to get professional experience as a web developer. I have plenty of hobbyist experience with
-    programming in general, and I also taught HTML/CSS/JS professionally, but I never worked directly as a web developer.
-    I would love to take up the opportunity to design and build user interfaces that meet UCI's high standards, while taking steps towards
+    <p>I'm applying to this position to get professional experience as a software developer. I have plenty of hobbyist experience with
+    programming in general, and I also teach many programming languages professionally, such as Java, C#, and Python,
+    but I never worked directly as a software developer.
+    I would love to take up the opportunity to design and build software while taking steps towards
     building a career that I can be proud of.</p>
     <p>
-    Front-end development for me is a perfect mix between creativity and logic. Using the programming tools available to me,
+    Software development for me is a perfect mix between creativity and logic. Using the programming tools available to me,
     I can utilize them to bolster my creative expression. One such example of this is using Scalable Vector Graphics (SVG) to
     add an extra dimension of interactivity to any website, expressing strokes and shapes using code.
     </p>
